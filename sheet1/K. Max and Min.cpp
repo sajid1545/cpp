57b7f9988b5dea3e081fa5bd13922da941035a1a
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 20:41:06 +0600

    K ✅

---
+++ sheet1/K. Max and Min.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A, B, C;
+
+    int max, min;
+
+    cin >> A >> B >> C;
+
+    max = A;
+    if (B > max)
+        max = B;
+
+    if (C > max)
+        max = C;
+
+    min = A;
+    if (B < min)
+        min = B;
+
+    if (C < min)
+        min = C;
+
+    cout << min << " " << max;
+
+    return 0;
+}
