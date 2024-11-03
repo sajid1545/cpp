aee70fcb052bc6f1e32abfc34fd37b7c2218721e
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 13:20:18 +0600

    D problem solved ✅

--- sheet1/D. Difference.cpp
+++ sheet1/D. Difference.cpp
@@ -7,9 +7,9 @@ int main()
 {
     long long int A, B, C, D;
 
-    cin >> A, B, C, D;
+    cin >> A >> B >> C >> D;
 
-    int difference = (A * B) - (C * D);
+    long long int difference = (A * B) - (C * D);
 
     cout << "Difference = " << difference;
 
