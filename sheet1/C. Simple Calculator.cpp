54c3c43e43011a9c3ab0b675d02b7e98766c2fc6
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 11:22:35 +0600

    Problem C done ✅

---
+++ sheet1/C. Simple Calculator.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int num1, num2;
+
+    cin >> num1 >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+
+    return 0;
+}
