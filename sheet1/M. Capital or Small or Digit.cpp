e993e83d9ed68c6efe64e69d259bb42c61fe6ffe
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 21:23:24 +0600

    M ✅

---
+++ sheet1/M. Capital or Small or Digit.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char X;
+
+    cin >> X;
+
+    int AsciiValue = int(X);
+
+    if (AsciiValue >= 65 && AsciiValue <= 90)
+    {
+        cout << "ALPHA" << "\n"
+             << "IS CAPITAL";
+    }
+    else if (AsciiValue >= 97 && AsciiValue <= 122)
+    {
+        cout << "ALPHA" << "\n"
+             << "IS SMALL";
+    }
+    else if (AsciiValue >= 48 && AsciiValue <= 57)
+    {
+        cout << "IS DIGIT";
+    }
+
+    return 0;
+}
