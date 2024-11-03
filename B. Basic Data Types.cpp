cfa1e34e38dd3a2765ff2879c73a7c9c7635e28e
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 11:02:16 +0600

    Problem B solved

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    long long int l;
+    char ch;
+    float f;
+    double d;
+
+    cin >> i >> l >> ch >> f >> d;
+
+    cout << i << "\n"
+         << l << "\n"
+         << ch << "\n"
+         << f << "\n"
+         << d << endl;
+
+    return 0;
+}
