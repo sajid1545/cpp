099d0d969020f60ccf6b7c61ff9b5aab3841d3bb
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 10:47:33 +0600

    first commit

---
+++ Say Hello With C++.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string input;
+
+    getline(cin, input);
+
+    cout << "Hello, " << input;
+
+    return 0;
+}
