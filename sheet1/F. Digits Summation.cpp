687874120b248e5c99c86736ca426a6921920351
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-03 14:27:47 +0600

    F ✅

--- sheet1/F. Digits Summation.cpp
+++ sheet1/F. Digits Summation.cpp
@@ -5,10 +5,17 @@ using namespace std;
 
 int main()
 {
-
     string N, M;
 
     cin >> N >> M;
 
-        return 0;
+    char lastN = N.at(N.length() - 1);
+    char lastM = M.at(M.length() - 1);
+
+    long long int lastNDigit = lastN - '0';
+    long long int lastMDigit = lastM - '0';
+
+    cout << lastNDigit + lastMDigit;
+
+    return 0;
 }
