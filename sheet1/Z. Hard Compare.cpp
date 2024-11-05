77941a368882c123d1f4757c24b26bf5c0801959
Sajjad Abdullah <clashking1545@gmail.com>
2024-11-05 21:12:15 +0600

    Z ➖

--- sheet1/Z. Hard Compare.cpp
+++ sheet1/Z. Hard Compare.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 int main()
 {
-    long long A, C;
-    long long B, D;
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
 
-    cin >>
-        A >> B >> C >> D;
+    // Calculate B * log(A) and D * log(C) for comparison
+    double left_side = B * log(A);
+    double right_side = D * log(C);
 
-    cout << pow(A, B) << "   " << pow(C, D);
-
-    // if (pow(A, B) > pow(C, D))
-    // {
-    //     cout << "YES";
-    // }
-    // else
-    // {
-    //     cout << "NO";
-    // }
+    // Compare the two results
+    if (left_side > right_side)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
