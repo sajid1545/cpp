37d75aaf300184a0afd29ab819f3304b3eb831bb
Sajjad Abdullah <clashking1545@gmail.com>
2024-12-29 11:07:03 +0600

    refactor: simplify main function to add two numbers

--- practice.cpp
+++ practice.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// Find the sum of first n Natural Numbers
-
 int main()
 {
+    int x = 10;
+    int y = 20;
 
-    int X;
-
-    cout << "Enter a number : " << endl;
-
-    cin >> X;
-
-    int sum = 0;
-    cout << "The natural numbers upto" << X << "th terms are " << endl;
-    for (int i = 0; i <= X; i++)
-    {
-        sum = sum + i;
-        cout << i;
-    }
-
-    cout << "\n";
-
-    cout << "The sum of the natural numbers is: " << sum << endl;
-    return 0;
+    cout << x + y;
 }
