d5a887caa3203744b0fe6a3dbe8503423aebe6ee
Sajjad Abdullah <clashking1545@gmail.com>
2025-03-04 15:14:06 +0600

    :delete: remove old Hello World example and add new student information example in C

---
+++ learning/c/basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int studentId = 1;
+    int studentAge = 25;
+    float studentFee = 22.22;
+    char studentGrade = 'A';
+
+    printf("Student id: %d\n", studentId);
+    printf("Student Age: %d\n", studentAge);
+    printf("Student Fee: %f\n", studentFee);
+    printf("Student Grade: %c\n", studentGrade);
+
+    return 0;
+}
